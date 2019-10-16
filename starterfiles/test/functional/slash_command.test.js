0c35693c54f273c86eaae0ca245cfb4cc6ccb241
Elijah Sattler <esattler@umich.edu>
2019-10-15 21:02:11 -0400

    Updated tests to work in Slack Community workspace

--- starterfiles/test/functional/slash_command.test.js
+++ starterfiles/test/functional/slash_command.test.js
@@ -22,9 +22,9 @@ describe('Starter Test', () => {
       await browser.close();
     });
   
-    it('should be titled "Slack | Spec Tester | Spec" after logging in and opening DMs', async () => {
+    it('should be titled "Slack | Spec Test Bot | Slack Community" after logging in and opening DMs', async () => {
       // Log in
-      await page.goto('https://spec-test-conf.slack.com');
+      await page.goto('https://community.slack.com');
       await expect(page).toFill('input[id="email"]', process.env.email, { timeout: TIMEOUT });
       await expect(page).toFill('input[id="password"]', process.env.password, { timeout: TIMEOUT });
       await expect(page).toClick('button[id="signin_btn"]', { timeout: TIMEOUT });
@@ -34,10 +34,10 @@ describe('Starter Test', () => {
       await expect(page).toClick(`a[data-qa-channel-sidebar-channel-id="${process.env.channel}"]`, { timeout: TIMEOUT });
       
       // Verify Page Title
-      expect(await page.title()).toBe('Slack | Spec Tester | Spec');
+      expect(await page.title()).toBe('Slack | Spec Test Bot | Slack Community');
     }, TIMEOUT);
 
-    it('should respond with "Hello world! after running /test_slash_command', async() => {
+    it('should respond with "Hello world!" after running /test_slash_command', async() => {
         // Execute Slash Command
         await expect(page).toFill('div[data-qa="message_input"]', '/test_slash_command', { timeout: TIMEOUT });
         await page.keyboard.press('Enter');
